81d9f571c7e658e49f3d965f9f5a802d376bbafe
Pragya Choudhary <your.pragyachoudhary759@gmail.com>
2018-03-11 01:29:00 +0530

    Enabling bluetooth without permission

---
+++ app/src/main/java/com/example/techolabz/ble_demo/Nav_bar.java
@@ -0,0 +1,56 @@
+package com.example.techolabz.ble_demo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.Locale;
+import android.widget.Toast;
+
+public class Nav_bar extends Activity {
+    TextToSpeech t1;
+
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_bar);
+
+        b1=(Button)findViewById(R.id.button);
+
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = getIntent();
+                String toSpeak = Intent.intent.getExtras().getString("epuzzle");
+                Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    public void onPause(){
+        if(t1 !=null){
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
+    }
+}
+
+
